c6a2ceeb55a597be9c3254ba078d1c066f0b79c7
Jesus Garcia <fairplaay16@gmail.com>
2019-06-28 17:19:37 -0400

    scafolding created

---
+++ README.md
@@ -0,0 +1,16 @@
+Scafold MERN + DOCKER
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `docker-compose up -d`
+
+Runs the frontend react js in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+Runs the backend express in the development mode.<br>
+Open [http://localhost:3001](http://localhost:3000) to view it in the browser.
+
+Runs the database<br>
+[mongodb://localhost:27018/api](mongodb://localhost:27018/api).
---
+++ api/src/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+// import authRoutes from './routes/authentication';
+
+// config .env
+dotenv.config();
+
+// app
+const app = express();
+
+// connect to db
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('conected to db'));
+
+// middlewares
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-PINGOTHER,Content-Type,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization');
+    next();
+});
+
+// routes middlewares
+app.use('/', (req, res) => res.send('its works'));
+// run app on port
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`Server up and running in port ${process.env.PORT}`);
+});
